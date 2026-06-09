4ab9a229a1e6a7657100cb3aed3c298bb8abe8f3
Arley_Fernando_Torres_Galindo <106280173+ArleyFernandoTorresG@users.noreply.github.com>
2026-06-09 13:54:36 -0500

    una prueba

--- ch03/find_max.py
+++ ch03/find_max.py
@@ -21,8 +21,8 @@
 
 def find_max(data):
   """Return the maximum element from a nonempty Python list."""
-  biggest = data[0]               # The initial value to beat
+  biggest1 = data[0]               # The initial value to beat
   for val in data:                # For each value:
-    if val > biggest:              # if it is greater than the best so far,
-      biggest = val               # we have found a new best (so far)
-  return biggest                  # When loop ends, biggest is the max
+    if val > biggest1:              # if it is greater than the best so far,
+      biggest1 = val               # we have found a new best (so far)
+  return biggest1                  # When loop ends, biggest is the max
